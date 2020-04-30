e0e1e2144dbaa3ecf058a53004992edd7c162924
Sylvester Menawar <sylvesternathan93@gmail.com>
2020-04-30 20:45:01 +0100

    Updates

---
+++ precache-manifest.4fe23b9fdd4b84206736a99abd9fdf7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6a1c598392590f21766e2951d4dab5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4b601954ff5231a4a21",
+    "url": "/monsters-rolodex/static/css/main.bbdbb200.chunk.css"
+  },
+  {
+    "revision": "b81419f85758868b9ced",
+    "url": "/monsters-rolodex/static/js/2.62d78548.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.62d78548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b601954ff5231a4a21",
+    "url": "/monsters-rolodex/static/js/main.6a730630.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
